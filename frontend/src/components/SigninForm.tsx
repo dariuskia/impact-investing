ed89f112b99b9f66ff4cafac4f595bec6ed37074
Tony <totran0331@gmail.com>
2024-03-23 18:12:53 -0400

    Add SigninForm component

---
+++ frontend/src/components/SigninForm.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { signIn } from "next-auth/react";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().email().min(2, {
+    message: "Email must be at least 2 characters.",
+  }),
+});
+
+export function SigninForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    // Logic to sign in
+    const response = await signIn("email", {
+      email: values.email,
+      redirect: false,
+      callbackUrl: `/dashboard`,
+    });
+
+    console.log(response);
+    if (response?.ok) {
+      console.log("Sign in successful");
+    } else {
+      console.error("Sign in failed");
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Sign in
+        </Button>
+      </form>
+    </Form>
+  );
+}
