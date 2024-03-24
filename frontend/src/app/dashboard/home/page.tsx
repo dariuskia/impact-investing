fe6fd60ee80325b6236a5800cd32b744e1469d40
Tony <totran0331@gmail.com>
2024-03-24 05:38:13 -0400

    Add redirect logic for onboarding page

--- frontend/src/app/dashboard/home/page.tsx
+++ frontend/src/app/dashboard/home/page.tsx
@@ -1,19 +1,30 @@
 import { redirect } from "next/navigation";
 import Sidebar from "~/components/Sidebar";
+import Test from "~/components/Test";
 import { getServerAuthSession } from "~/server/auth";
 
 export default async function Home() {
   const session = await getServerAuthSession();
 
-  //   if (!session) {
-  //     redirect("/signin");
-  //   }
+  if (!session) {
+    redirect("/signin");
+  }
+
+  if (!session.user.onboarded) {
+    redirect("/onboard");
+  }
 
   return (
     <main className="flex min-h-screen w-full bg-neutral-50 text-neutral-900">
-      <div className="w-full max-w-md">
+      {/* <div className="relative flex w-full max-w-7xl grid-cols-2"> */}
+      <div className="w-[248px]">
         <Sidebar />
       </div>
+      <div className="mx-auto flex h-full min-h-screen w-full max-w-xl flex-1 py-10 md:py-16">
+        dasdsa
+      </div>
+      {/* <Test /> */}
+      {/* </div> */}
     </main>
   );
 }
