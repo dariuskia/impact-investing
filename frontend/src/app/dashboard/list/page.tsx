0c5cbc50943849e79f82f120250dd278c9332b9b
Tony <totran0331@gmail.com>
2024-03-24 05:54:40 -0400

    Remove DashboardLayout and add MediaList and RadioGroup

---
+++ frontend/src/app/dashboard/list/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from "next/navigation";
+import Sidebar from "~/components/Sidebar";
+import { getServerAuthSession } from "~/server/auth";
+
+export default async function MediaList() {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    redirect("/signin");
+  }
+
+  if (!session.user.onboarded) {
+    redirect("/onboard");
+  }
+
+  return (
+    <main className="flex min-h-screen w-full bg-neutral-50 text-neutral-900">
+      <div className="w-[248px]">
+        <Sidebar />
+      </div>
+      <div className="mx-auto flex h-full min-h-screen w-full max-w-xl flex-1 py-10 md:py-16">
+        dasdsa
+      </div>
+    </main>
+  );
+}
