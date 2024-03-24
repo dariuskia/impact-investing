5c212ace8cb5790a9f83b59f509a9c19f62cde9c
Tony <totran0331@gmail.com>
2024-03-24 05:53:06 -0400

    Commented out unused code in page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -66,17 +66,17 @@ async function CrudShowcase() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  const latestPost = await api.post.getLatest();
+  // const latestPost = await api.post.getLatest();
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
+      {/* {latestPost ? (
         <p className="truncate">Your most recent post: {latestPost.name}</p>
       ) : (
         <p>You have no posts yet.</p>
-      )}
+      )} */}
 
-      <CreatePost />
+      {/* <CreatePost /> */}
     </div>
   );
 }
